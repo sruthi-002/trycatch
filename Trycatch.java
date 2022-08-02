bfef9d105e23b9e67369e55faa067ca902a31db0
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-02 11:45:39 +0530

    Add files via upload

---
+++ Trycatch.java
@@ -0,0 +1,28 @@
+﻿package com.company;
+import java.util.Scanner;
+class trycatch
+{
+   public  static void trycatchex()
+   {
+       int n;
+       float avg;
+       int sum;
+       Scanner in = new Scanner(System.in);
+       n = in.nextInt();
+       sum =in.nextInt();
+       try {
+          avg = sum/n;
+       }
+       catch(ArithmeticException e)
+       {
+           System.out.println("Please enter the valid number");
+       }
+   }
+}
+public class Main {
+
+    public static void main(String[] args) {
+	trycatch n = new trycatch();
+    n.trycatchex();
+    }
+}
